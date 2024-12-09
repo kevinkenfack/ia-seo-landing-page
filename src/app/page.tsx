79787bd225d255f155088b1f80f28beda8c3dd24
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2024-12-09 17:01:34 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import CallToAction from '@/sections/CallToAction'
+import { Features } from '@/sections/Features'
+import Footer from '@/sections/Footer'
+import Header from '@/sections/Header'
+import Hero from '@/sections/Hero'
+import LogoTicker from '@/sections/LogoTicker'
+import { Testimonials } from '@/sections/Testimonials'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <Header/>
+      <Hero/>
+      <LogoTicker/>
+      <Features/>
+      <Testimonials/>
+      <CallToAction/>
+      <Footer/>
+    </>
+  )
+}
+
+export default page
+
---
+++ src/sections/Header.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import LogoIcon from "@/assets/logo.svg";
+import MenuIcon from "@/assets/icon-menu.svg";
+import Button from "@/components/Button";
+
+const Header = () => {
+  return (
+    <header className="py-4 border-b border-white/15 md:border-none sticky top-0 z-10">
+      <div className="absolute inset-0 backdrop-blur -z-10 md:hidden"></div>
+      <div className="container">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto relative">
+        <div className="absolute inset-0 backdrop-blur -z-10 hidden md:block"></div>
+          <div>
+            <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15">
+              <LogoIcon className="h-8 w-8 text-white"/>
+            </div>
+          </div>
+          <div className="hidden md:block">
+            <nav className="flex gap-8 text-sm">
+              <a className="text-white/70 hover:text-white transition" href="">Features</a>
+              <a className="text-white/70 hover:text-white transition" href="">Developers</a>
+              <a className="text-white/70 hover:text-white transition" href="">Pricing</a>
+              <a className="text-white/70 hover:text-white transition" href="">changelog</a>
+            </nav>
+          </div>
+          <div className="flex gap-4 items-center">
+            <Button>Join waitlist</Button>
+            <MenuIcon className="md:hidden"/>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
